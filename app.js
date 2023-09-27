4302cdf67b6e4e8db23402fd9b9f9fc1e62ce5b9
Kush Kumar <kush6279@gmail.com>
2023-09-28 00:54:16 +0530

    about and contact

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const ejs = require("ejs");
 const _= require("lodash");
 
 const homeStartingContent = "Each new day brings with it a sense of possibility and opportunity.Every day offers a chance to learn something new. Whether it's a new skill, a new piece of knowledge, or a new perspective, continuous learning keeps our minds engaged and growing.The people we connect with on our journey—family, friends, mentors, and even strangers—enrich our lives and provide us with love, support, and companionship.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const aboutContent = "This is place where I write all the learing of my day please keep following it ";
+const contactContent = "Kush Kumar  contact no: 878787878787 email:kush@gmail.com";
 
 const app = express();
 
